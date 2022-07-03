0495e58207ca30cf7d0968c2324cbada7aa32f3b
Tobi <twawrik@yahoo.de>
2022-07-03 20:38:32 +0200

    Calculation.ini to write Mods and Weaopns there for calculation

--- Calculation.py
+++ Calculation.py
@@ -7,7 +7,6 @@ from Damage import Damage
 from Enemy import Enemy
 from DamageTypes import DamageTypes
 import configparser
-import sys
 
 def loadWeapon(name: str):
     weaponParser = configparser.ConfigParser()
@@ -19,60 +18,69 @@ def loadWeapon(name: str):
         weaponStats.Damage[entry] = float(weaponParser["Weapon"][entry])
     return weaponStats
 
-if len(sys.argv) == 1:
-    print("Usage: python Calculation.py \"WeaponName_1\" \"WeaponName_n\"")
+def loadMod(name: str):
+    modParser = configparser.ConfigParser()
+    modParser.read("Mods/" + name + ".ini")
+    mods = Mods()
+    for entry in DamageTypes().Multiplier:
+        mods.Multiplier[entry] = float(modParser["Mods"][entry])
+    return mods
 
-weaponStats = []
+weaponStatList = []
+modStatList = []
 
-for name in sys.argv[1::]:
-    weaponStats.append(loadWeapon(str(name)))
+calculationParser = configparser.ConfigParser()
+calculationParser.read("Calculation.ini")
+for entry in calculationParser["Weapons"]:
+    weaponStatList.append(loadWeapon(str(calculationParser["Weapons"][entry])))
 
-modParser = configparser.ConfigParser()
-modParser.read("Mods/Modconfig_1.ini")
-mods = Mods()
-
-for entry in DamageTypes().Multiplier:
-    mods.Multiplier[entry] = float(modParser["Mods"][entry])
+for entry in calculationParser["Mods"]:
+    modStatList.append(loadMod(str(calculationParser["Mods"][entry])))
 
 weapons = []
-for stat in weaponStats:
-    weapons.append(Weapon(stat, mods))
+
+if len(weaponStatList) != len(modStatList):
+    print("Uneven length of weapons and mods!")
+
+else:
+    for i in range(0, len(weaponStatList)):
+        weapons.append(Weapon(weaponStatList[i], modStatList[i]))
 
 
-sarpaMods = Mods()
-sarpaMods.Multiplier["BaseDamage"] = 4.85 # 4x Condition Overload
-sarpaMods.Multiplier["CritChance"] = 4.8 # fully stacked blood rush
-sarpaMods.Multiplier["CritDamage"] = 0.9 # Organ Shatter
-sarpaMods.Multiplier["Viral"] = 1.8 # 90 Tox 90 Cold
+    sarpaMods = Mods()
+    sarpaMods.Multiplier["BaseDamage"] = 4.85 # 4x Condition Overload
+    sarpaMods.Multiplier["CritChance"] = 4.8 # fully stacked blood rush
+    sarpaMods.Multiplier["CritDamage"] = 0.9 # Organ Shatter
+    sarpaMods.Multiplier["Viral"] = 1.8 # 90 Tox 90 Cold
 
-gunnerArmor = Armor()
-gunnerArmor.ArmorMultiplier["Corrosive"] = 0.75
-gunnerArmor.ArmorMultiplier["Puncture"] = 0.5
-gunnerArmor.ArmorMultiplier["Blast"] = -0.25
-gunnerArmor.ArmorMultiplier["Slash"] = -0.25
+    gunnerArmor = Armor()
+    gunnerArmor.ArmorMultiplier["Corrosive"] = 0.75
+    gunnerArmor.ArmorMultiplier["Puncture"] = 0.5
+    gunnerArmor.ArmorMultiplier["Blast"] = -0.25
+    gunnerArmor.ArmorMultiplier["Slash"] = -0.25
 
-gunnerHealth = Health()
-gunnerHealth.HealthMultiplier["Gas"] = -0.5
-gunnerHealth.HealthMultiplier["Impact"] = -0.25
-gunnerHealth.HealthMultiplier["Heat"] = 0.25
-gunnerHealth.HealthMultiplier["Slash"] = 0.25
-gunnerHealth.HealthMultiplier["Viral"] = 0.75
+    gunnerHealth = Health()
+    gunnerHealth.HealthMultiplier["Gas"] = -0.5
+    gunnerHealth.HealthMultiplier["Impact"] = -0.25
+    gunnerHealth.HealthMultiplier["Heat"] = 0.25
+    gunnerHealth.HealthMultiplier["Slash"] = 0.25
+    gunnerHealth.HealthMultiplier["Viral"] = 0.75
 
-gunner = Enemy(gunnerArmor, gunnerHealth)
+    gunner = Enemy(gunnerArmor, gunnerHealth)
 
-damage = []
-for weapon in weapons:
-    damage.append(Damage(weapon, gunner))
+    damage = []
+    for weapon in weapons:
+        damage.append(Damage(weapon, gunner))
 
-#print("Hek Stats: \n" + HekWeap.ShowStats())
-#print("Strun Stats: \n" + StrunWeap.ShowStats())
-#print("Sarpa Stats: \n" + SarpaWeap.ShowStats())
+    #print("Hek Stats: \n" + HekWeap.ShowStats())
+    #print("Strun Stats: \n" + StrunWeap.ShowStats())
+    #print("Sarpa Stats: \n" + SarpaWeap.ShowStats())
 
-#procs = SarpaWeap.CalculateProcs()
-#sarpaDmg.CalculateSlashDamage()
+    #procs = SarpaWeap.CalculateProcs()
+    #sarpaDmg.CalculateSlashDamage()
 
-for entry in damage:
-    print(entry.weapon.Name + "Projectile: " + format(entry.CalculateSingleshot(), ",f"))
-    print(entry.weapon.Name + "Projectile: " + format(entry.CalculateMultishot(), ",f"))
-    if entry.weapon.Name == "Sarpa":
-        entry.CalculateSlashDamage()
+    for entry in damage:
+        print(entry.weapon.Name + "Projectile: " + format(entry.CalculateSingleshot(), ",f"))
+        print(entry.weapon.Name + "Projectile: " + format(entry.CalculateMultishot(), ",f"))
+        if entry.weapon.Name == "Sarpa":
+            entry.CalculateSlashDamage()
