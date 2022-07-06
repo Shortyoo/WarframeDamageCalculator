b0ab4cbe8ff5b3942373c2f91cfde40bdfa8b58e
Tobi <twawrik@yahoo.de>
2022-07-06 20:01:29 +0200

    Stats now has load-function

--- Calculation.py
+++ Calculation.py
@@ -1,8 +1,6 @@
 from Weapon import Weapon
-from Armor import Armor
 from Stats import Stats
 from Mods import Mods
-from Health import Health
 from Damage import Damage
 from Shield import Shield
 from Enemy import Enemy
@@ -17,73 +15,6 @@ parser.add_argument("-s", "--slash", help ="Calculates the damage of all slash p
 parser.add_argument("-sP", "--showProcs", help ="Calculates the probability to apply a proc on the opponent", action="store_true")
 args = parser.parse_args()
 
-def loadWeapon(name: str):
-    weaponParser = configparser.ConfigParser()
-    weaponParser.read("Weapons/"+name+".ini")
-    weaponStats = Stats(name)
-    for entry in DamageTypes().Multiplier:
-        if entry == "BaseDamage" or entry == "FactionDamage":
-            continue
-        weaponStats.Damage[entry] = float(weaponParser["Weapon"][entry])
-    return weaponStats
-
-def loadMod(name: str):
-    modParser = configparser.ConfigParser()
-    modParser.read("Mods/" + name + ".ini")
-    mods = Mods(name)
-    for entry in DamageTypes().Multiplier:
-        mods.Multiplier[entry] = float(modParser["Mods"][entry])
-    return mods
-
-def loadEnemy(name: str):
-    enemyParser = configparser.ConfigParser()
-    enemyParser.read("Enemies/" + name + ".ini")
-
-    hp = int(enemyParser["Base"]["HP"])
-    armor = int(enemyParser["Base"]["Armor"])
-    shield = int(enemyParser["Base"]["Shield"])
-    baseLevel = int(enemyParser["Base"]["BaseLevel"])
-    level = int(enemyParser["Base"]["Level"])
-    name = str(enemyParser["Base"]["Name"])
-
-    if level - baseLevel < 70:
-        armor = round(armor * (1 + 0.0005 * (level - baseLevel)**1.75), 2)
-        hp = round(hp * (1 + 0.015 * (level - baseLevel)**2), 2)
-        shield = round(shield * (1 + 0.02*(level - baseLevel)**1.75), 2)
-    else:
-        armor = round(armor * (1 + 0.4 * (level - baseLevel)**0.75), 2)
-        hp = round(hp * (1 + (24*(5**0.5)/5) * (level - baseLevel) ** 0.5), 2)
-        shield = round(shield * (1 + 1.6*(level - baseLevel)**0.75), 2)
-
-    if int(enemyParser["Base"]["SteelPath"]) == 1:
-        # at first, add the armor value that it gains from + 100 levels
-        # https://warframe.fandom.com/wiki/The_Steel_Path
-
-        hp = hp * 2.5
-        armor = armor * 2.5
-        shield = shield * 2.5
-
-    gunnerArmor = Armor()
-    for entry in DamageTypes().Damage:
-        gunnerArmor.ArmorMultiplier[entry] = float(enemyParser["Armor"][entry])
-
-    gunnerHealth = Health()
-    for entry in DamageTypes().Damage:
-        gunnerHealth.HealthMultiplier[entry] = float(enemyParser["Health"][entry])
-
-    gunnerShield = Shield()
-    for entry in DamageTypes().Damage:
-        gunnerShield.ShieldMultiplier[entry] = float(enemyParser["Shield"][entry])
-
-    gunnerStatus = Status()
-    for entry in DamageTypes().Damage:
-        gunnerStatus.Status[entry] = float(enemyParser["ActiveProcs"][entry])
-
-    gunnerStatus.Status["CorrosiveProjection"] = enemyParser["ActiveProcs"]["CorrosiveProjection"]
-
-    return Enemy(gunnerHealth, gunnerArmor, gunnerShield, hp, armor, shield, gunnerStatus, name)
-
-
 weaponStatList = []
 modStatList = []
 enemyList = []
@@ -91,13 +22,13 @@ enemyList = []
 calculationParser = configparser.ConfigParser()
 calculationParser.read("Calculation.ini")
 for entry in calculationParser["Weapons"]:
-    weaponStatList.append(loadWeapon(str(calculationParser["Weapons"][entry])))
+    weaponStatList.append(Stats.loadWeapon(str(calculationParser["Weapons"][entry])))
 
 for entry in calculationParser["Mods"]:
-    modStatList.append(loadMod(str(calculationParser["Mods"][entry])))
+    modStatList.append(Mods.loadMod(str(calculationParser["Mods"][entry])))
 
 for entry in calculationParser["Enemy"]:
-    enemyList.append(loadEnemy(str(calculationParser["Enemy"][entry])))
+    enemyList.append(Enemy.loadEnemy(str(calculationParser["Enemy"][entry])))
 
 if len(weaponStatList) != len(modStatList) or len(weaponStatList) != len(enemyList) or len(modStatList) != len(enemyList):
     print("Unmatching list-lengths!!")
@@ -122,10 +53,10 @@ else:
 
     for entry in damage:
         print("Quanta: " + str(entry.weapon.Quantum()))
-        #entry.ShootEnemy()
-        damage = entry.CalculateRawDamage()
-        print(entry.BuildString() + " Damage against Shield: " + format(damage[0], ",f"))
-        print(entry.BuildString() + " Damage against Health: " + format(damage[1], ",f"))
+        entry.ShootEnemy()
+        dmg = entry.CalculateRawDamage()
+        print(entry.BuildString() + " Damage against Shield: " + format(dmg[0], ",f"))
+        print(entry.BuildString() + " Damage against Health: " + format(dmg[1], ",f"))
         firerate = entry.weapon.stats.Damage["FireRate"]
         magsize = entry.weapon.stats.Damage["MagSize"]
         DPS = magsize / firerate
