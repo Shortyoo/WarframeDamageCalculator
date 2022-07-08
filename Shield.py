dda29332febf8c1fe6f915ea4d933751f257e087
Tobi Laptop <twawrik@yahoo.de>
2022-07-08 22:33:03 +0200

    Used DamageTypesInstance in Shield.py

--- Shield.py
+++ Shield.py
@@ -3,5 +3,6 @@ from DamageTypes import DamageTypes
 class Shield:
     def __init__(self):
         self.ShieldMultiplier = {}
-        for entry in DamageTypes().Damage:
+        self.DamageTypesInstance = DamageTypes()
+        for entry in DamageTypesInstance.Damage:
             self.ShieldMultiplier[entry] = 0
